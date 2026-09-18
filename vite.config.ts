129a024d379d2bbd7e57f4ce109eabf5fb6acab5
Jabes <jabeslima15@gmail.com>
2026-09-18 12:37:48 -0300

    Update dploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-import { base } from 'framer-motion/client';
+
 
 // https://vite.dev/config/
 export default defineConfig({
